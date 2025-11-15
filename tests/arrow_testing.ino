970acae0cbdccbf810399b05e313064bc9896ff7
Ze Rato <132148561+Ze-Rato@users.noreply.github.com>
2025-11-15 13:03:34 +0000

    Chnaged reference point in arrow functions and started working on animation logic

--- tests/arrow_testing.ino
+++ tests/arrow_testing.ino
@@ -7,6 +7,12 @@
 // Arrows stuff
 #define ARROW_WIDTH 15
 #define ARROW_HEIGHT 20
+#define ARROW_LEFT_X 95
+#define ARROW_UP_X 75
+#define ARROW_DOWN_X 55
+#define ARROW_RIGHT_X 35
+#define ARROW_START_HEIGHT 10
+#define ARROW_END_HEIGHT 140
 
 TFT TFTscreen = TFT(cs, dc, rst);
 // position of the line on screen
@@ -21,20 +27,27 @@ void setup() {
   TFTscreen.background(0, 0, 0);
   TFTscreen.setRotation(2);
 
-  //A cor usada nas funnções fillRect e fillTriangle está no formato BGR565
-  ArrowLeft(125, 50, ARROW_WIDTH, ARROW_HEIGHT, 0x28FF);
-  ArrowUp(95, 50, ARROW_WIDTH, ARROW_HEIGHT, 0x07FF);
-  ArrowDown(75, 50, ARROW_WIDTH, ARROW_HEIGHT, 0x0640);
-  ArrowRight(45, 50, ARROW_WIDTH, ARROW_HEIGHT, 0xF800);
-
+  //Check colors at: https://rgbcolorpicker.com/565
+  ArrowLeft(ARROW_LEFT_X, ARROW_START_HEIGHT, ARROW_WIDTH, ARROW_HEIGHT, 0x28FF);
+  ArrowUp(ARROW_UP_X, ARROW_START_HEIGHT, ARROW_WIDTH, ARROW_HEIGHT, 0x07FF);
+  ArrowDown(ARROW_DOWN_X, ARROW_START_HEIGHT, ARROW_WIDTH, ARROW_HEIGHT, 0x0640);
+  ArrowRight(ARROW_RIGHT_X, ARROW_START_HEIGHT, ARROW_WIDTH, ARROW_HEIGHT, 0xF800);
+ 
+  ArrowLeft(ARROW_LEFT_X, ARROW_END_HEIGHT, ARROW_WIDTH, ARROW_HEIGHT, 0xC618);
+  ArrowUp(ARROW_UP_X, ARROW_END_HEIGHT, ARROW_WIDTH, ARROW_HEIGHT, 0xC618);
+  ArrowDown(ARROW_DOWN_X, ARROW_END_HEIGHT, ARROW_WIDTH, ARROW_HEIGHT, 0xC618);
+  ArrowRight(ARROW_RIGHT_X, ARROW_END_HEIGHT, ARROW_WIDTH, ARROW_HEIGHT, 0xC618);
+ 
   //int a = TFTscreen.width();
   //Serial.print(String(a));
   //TFTscreen.stroke(255, 0, 0);
   //TFTscreen.line(0, 0, a/2, 0);
 }
 
-void ArrowDown(int x, int y, int width, int height, uint16_t color) {
-  //Nota: As coordenadas x, y correspondem à ponta da seta (ponta do triangulo)
+void ArrowDown(int x1, int y1, int width, int height, uint16_t color) {
+  int x = x1;
+  int y = y1 - height/2;
+  //Nota: As coordenadas x, y correspondem à ponta da seta (ponta do triangulo), enqaunto que as x1 e y1 devem corresponder ao centro (vertical) da seta
 
   //Proporções
   int rectheight = height * 2 / 3;  //2/3 da altura total é a altura do retangulo
@@ -46,7 +59,7 @@ void ArrowDown(int x, int y, int width, int height, uint16_t color) {
   
   /*  Pontos de interesse (canto do retangulo e vertices do triangulo) [PARA DEBUG]
   TFTscreen.stroke(255, 255, 255);
-  TFTscreen.point(x, y);
+  TFTscreen.point(x1, y1);
   TFTscreen.stroke(0, 255, 0);
   TFTscreen.point(x - width/4, y + triheight);
   TFTscreen.point(x - width/2, y + triheight);
@@ -54,9 +67,11 @@ void ArrowDown(int x, int y, int width, int height, uint16_t color) {
   */
 }
 
-void ArrowUp(int x, int y, int width, int height, uint16_t color) {
-  //Nota: As coordenadas x, y correspondem à ponta da seta (ponta do triangulo)
+void ArrowUp(int x1, int y1, int width, int height, uint16_t color) {
+//Nota: As coordenadas x, y correspondem à ponta da seta (ponta do triangulo), enqaunto que as x1 e y1 devem corresponder ao centro (vertical) da seta
 
+  int x = x1;
+  int y = y1 + height/2;
   //Proporções
   int rectheight = height * 2 / 3;  //2/3 da altura total é a altura do retangulo
   int triheight = height * 1/3;  //1/3 da altura total é a altura do triangulo
@@ -67,7 +82,7 @@ void ArrowUp(int x, int y, int width, int height, uint16_t color) {
 
   /*  Pontos de interesse (canto do retangulo e vertices do triangulo) [PARA DEBUG]
   TFTscreen.stroke(255, 255, 255);
-  TFTscreen.point(x, y);
+  TFTscreen.point(x1, y1);
   TFTscreen.stroke(0, 255, 0);
   TFTscreen.point(x - width/4, y - triheight);
   TFTscreen.point(x - width/2, y - triheight);
@@ -75,9 +90,10 @@ void ArrowUp(int x, int y, int width, int height, uint16_t color) {
   */
 }
 
-void ArrowLeft(int x, int y, int width, int height, uint16_t color) {
-  //Nota: As coordenadas x, y correspondem à ponta da seta (ponta do triangulo)
-
+void ArrowLeft(int x1, int y1, int width, int height, uint16_t color) {
+  //Nota: As coordenadas x, y correspondem à ponta da seta (ponta do triangulo), enqaunto que as x1 e y1 devem corresponder ao centro (vertical) da seta
+  int x = x1 + height/2;
+  int y = y1;
   //Proporções
   int rectheight = height * 2 / 3;  //2/3 da altura total é a altura do retangulo
   int triheight = height * 1/3;  //1/3 da altura total é a altura do triangulo
@@ -88,7 +104,7 @@ void ArrowLeft(int x, int y, int width, int height, uint16_t color) {
   
   /*  Pontos de interesse (canto do retangulo e vertices do triangulo) [PARA DEBUG]
   TFTscreen.stroke(255, 255, 255);
-  TFTscreen.point(x, y);
+  TFTscreen.point(x1, y1);
   TFTscreen.stroke(0, 255, 0);
   TFTscreen.point(x - triheight, y + width/2);
   TFTscreen.point(x - triheight, y - width/2);
@@ -96,9 +112,10 @@ void ArrowLeft(int x, int y, int width, int height, uint16_t color) {
   */
 }
 
-void ArrowRight(int x, int y, int width, int height, uint16_t color) {
-  //Nota: As coordenadas x, y correspondem à ponta da seta (ponta do triangulo)
-
+void ArrowRight(int x1, int y1, int width, int height, uint16_t color) {
+//Nota: As coordenadas x, y correspondem à ponta da seta (ponta do triangulo), enqaunto que as x1 e y1 devem corresponder ao centro (vertical) da seta
+  int x = x1 - height/2;
+  int y = y1;
   //Proporções
   int rectheight = height * 2 / 3;  //2/3 da altura total é a altura do retangulo
   int triheight = height * 1/3;  //1/3 da altura total é a altura do triangulo
@@ -106,10 +123,10 @@ void ArrowRight(int x, int y, int width, int height, uint16_t color) {
   //Desenho da seta
   TFTscreen.fillRect(x + triheight, y - width/4, rectheight, width/2, color);
   TFTscreen.fillTriangle(x, y ,x + triheight, y + width/2, x + triheight, y - width/2, color);
-  
+
   /*  Pontos de interesse (canto do retangulo e vertices do triangulo) [PARA DEBUG]
   TFTscreen.stroke(255, 255, 255);
-  TFTscreen.point(x, y);
+  TFTscreen.point(x1, y1);
   TFTscreen.stroke(0, 255, 0);
   TFTscreen.point(x + triheight, y + width/2);
   TFTscreen.point(x + triheight, y - width/2);
@@ -117,20 +134,19 @@ void ArrowRight(int x, int y, int width, int height, uint16_t color) {
   */
 }
 
-void ArrowatTime(int type, long absolute_time){
+int Posanim(unsigned long startTime, unsigned long endTime){
+  //No final era bom ficar com uma função tipo ArrowatTime(startTime, endTime) que já tenham um loop (ou switch) em si
+  unsigned long now = millis();
+    
+  if (now <= startTime) return 0;
+  if (now >= endTime) return 180;
 
+  return map(now, startTime, endTime, 0, 180);
 }
 
 int y=50;
 void loop() {
-  /* Funciona.. mas é preciso generalizzar, e talvez dê jeito utilizar o tempo (absoluto) de quandoa  seta deve pressionada
-  int arrowHeight = 30;
-  ArrowDown(50, y, 20, 30, 0x0000);
-  
-  y--;
-
-  if (y < 0 - arrowHeight) y = TFTscreen.height(); // loop back
-  ArrowDown(50, y, 20, 30, 0x000F); //background color previous arrow (so it doesn't leave a trail)
-  delay(50);
-  */
+  int posY = Posanim(10000, 15000);
+  ArrowLeft(ARROW_LEFT_X, posY, ARROW_WIDTH, ARROW_HEIGHT, 0x28FF);
+  ArrowLeft(ARROW_LEFT_X, posY-1, ARROW_WIDTH, ARROW_HEIGHT, 0x0000);
 }
